779f433206e147a7001007d180479e8d5133e23e
ChopinCorentinpro <62313186+ChopinCorentinpro@users.noreply.github.com>
2020-03-22 01:26:40 +0100

    MAJ 22032020
    Update on GitHub

--- js/fonctionmenu.js
+++ js/fonctionmenu.js
@@ -1,5 +1,27 @@
-function myfunction() {       
-    var elmnt = document.getElementById("experiencepro");
-    elmnt.scrollIntoView();
-  }
+function presentation() {       
+  var elmnt = document.getElementById("num");
+  elmnt.scrollIntoView();
+  var active = document.getElementById("menu")
+  active.style.visibility =('visible');
+  var img = document.getElementById("imenu")
+  img.style.visibility =('visible')
+  var del = document.getElementsByClassName("next")
+  del.style.visibility = ('hidden')
+}
+
+function alerte() {
+alert("Lien indisponnible !")
+}
+setTimeout(function() {
+  // Code…
+var time = document.getElementById('msg');
+time.style.visibility = ('visible');
+var timea = document.getElementById('tmsg');
+timea.style.visibility = ('visible');
+}, 5000);
+function Dem() {
+  var rafraiche = document.getElementsByClassName("pmoi");
+  rafraiche.scrollIntoView();
+}
+
 
